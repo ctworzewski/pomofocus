2cb09e96a53f1e05a224f90e70be991d065e4bfc
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-07-26 11:01:27 +0200

    Commit test click button START

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Test')
+console.log('Test');
+
+// function startApp() {
+//     console.log('Działa przycisk')
+// }
+const startBtn = document.querySelector('.start-btn');
+
+function clickTest() {
+    console.log('Test klikania');
+}
+startBtn.addEventListener('click', clickTest);
