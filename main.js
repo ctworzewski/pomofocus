193b3b4ccaba54c20c04abb4e550e09237301499
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-07-27 11:21:47 +0200

    Counter is better make version_1

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ console.log('Test');
 const startBtn = document.querySelector('.start-btn');
 const stopBtn = document.querySelector('.stop-btn');
 const myCounter = document.querySelector('.my-counter');
-const paragraphTimer = document.createElement('div');
+// const paragraphTimer = document.createElement('.paragraphTimer');
+const paragr = document.querySelector('.paragraphTimer');
 
-const div = document.querySelector('.my-counter')
-myCounter.appendChild(paragraphTimer);
+// const div = document.querySelector('.my-counter')
+myCounter.appendChild(paragr);
 
 
 let counter = 60;
@@ -15,29 +16,24 @@ function startTest() {
 }
 
 function updateCounter(count) {
-    paragraphTimer.innerHTML = counter;
+    paragr.innerHTML = `25:${counter}`;
+    clearInterval(stopTest);
 }
 
-function stopApp() {
-    if (stopBtn) {
-        clearInterval(startTest);
-    }
+function stopTest() {
+    counter = 0;
+    return counter;
 }
+
+// function stopApp() {
+//     if (stopBtn) {
+//         clearInterval(startTest);
+//     }
+// }
 
 //  setInterval(startApp, 1000);
  setInterval(startTest, 1000);
 // startBtn.addEventListener('click', startApp);
 startBtn.addEventListener('click', startTest);
 
-stopBtn.addEventListener('click', stopApp);
-
-
-
-// function timeDown() {
-//     let  allSec = 59;
-//     for (let i = 0; i< allSec; i++) {
-//         // allSec = allSec + 1;
-//         console.log(allSec)
-//     }
-// }
-// setInterval(timeDown, 1000);
+stopBtn.addEventListener('click', stopTest);
