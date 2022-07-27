86ae1e107af5f7b82013b44d29f653a4000d6efc
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-07-27 19:06:07 +0200

    Correct repair v2

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 const startBtn = document.querySelector('.start-btn');
-const stopBtn = document.querySelector('.stop-btn');
+// const stopBtn = document.querySelector('.stop-btn');
 const myCounter = document.querySelector('.my-counter');
 const paragr = document.querySelector('.paragraphTimer');
-let counter = 11;
 
 myCounter.appendChild(paragr);
 
+let counter = 59;
 function startTest() {
     updateCounter(counter--);
+    // counter = 0;
 }
 function updateCounter(count) {
-    setInterval(startTest, 1000);
-    if (counter < 10){
-        paragr.innerHTML = `25:0${counter}`;
-    }
-    if (counter <= 0) {
-        counter = 0;
-        paragr.innerHTML = `25:0${counter}`;
-    }
-}
+    if (counter >= 10){
+        paragr.innerHTML = `25:${counter}`;
+    } else if(counter<10) {
 
+        paragr.innerHTML = `25:0${counter}`;
+    } else if(counter === 0) {
+        counter = 0;
+    }
+    // if (counter <= 0) {
+    //     counter = 0;
+    //     paragr.innerHTML = `25:0${counter}`;
+    // }
+    // setInterval(startTest, 1000);
+}
 
 startBtn.addEventListener('click', startTest);
