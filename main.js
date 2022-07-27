5789e333c46b5ece3accb072540004b5e1fdcd3b
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-07-27 09:14:22 +0200

    Add interaction to DOM, i must correct

--- main.js
+++ main.js
@@ -3,21 +3,28 @@ console.log('Test');
 const startBtn = document.querySelector('.start-btn');
 const myCounter = document.querySelector('.my-counter');
 
-function clickTest() {
+function startApp() {
+    let counter = 0;
+    const paragraphTimer = document.createElement('div');
+    // paragraphTimer.className('my-counter-paragraph');
+    paragraphTimer.innerText = `Czas: ${counter+=1}`;
+    // const div = document.querySelector('.my-counter')
+    myCounter.appendChild(paragraphTimer);
     console.log('Test klikania');
     let num=25;
-    for (let i = 0; i<=num; i++) {
+    for (let i = 0; i>num; i--) {
          if (num === 25) {
-            myCounter.innerHTML = i;
+            myCounter.innerHTML = num;
             console.log('my counter to: ', myCounter)
         }
         console.log('pokaż numer: ', i)
-        myCounter.innerHTML = i;
+        myCounter.innerHTML = num;
     }   
     
 }
 
-startBtn.addEventListener('click', clickTest);
+ setInterval(startApp, 1000);
+startBtn.addEventListener('click', startApp);
 
 
 
