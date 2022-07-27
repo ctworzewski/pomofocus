b3292899d2648f96f7cc3e1aba397659f89f4979
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-07-27 18:39:41 +0200

    Correct repair v1

---
+++ copyjs.js
@@ -0,0 +1,48 @@
+console.log('Test');
+
+const startBtn = document.querySelector('.start-btn');
+const stopBtn = document.querySelector('.stop-btn');
+const myCounter = document.querySelector('.my-counter');
+// const paragraphTimer = document.createElement('.paragraphTimer');
+const paragr = document.querySelector('.paragraphTimer');
+
+// const div = document.querySelector('.my-counter')
+myCounter.appendChild(paragr);
+
+
+let counter = 11;
+function startTest() {
+    updateCounter(counter--);
+}
+
+function updateCounter(count) {
+    if (counter < 10){
+        paragr.innerHTML = `25:0${counter}`;
+    }
+    // } else {
+
+        // paragr.innerHTML = `25:${counter}`;
+    // }
+    // if (counter === 0) {
+
+    //     clearInterval();
+    // }
+}
+
+// function stopTest() {
+//     counter = 0;
+//     return counter;
+// }
+
+// function stopApp() {
+//     if (stopBtn) {
+//         clearInterval(startTest);
+//     }
+// }
+
+//  setInterval(startApp, 1000);
+ setInterval(startTest, 1000);
+// startBtn.addEventListener('click', startApp);
+startBtn.addEventListener('click', startTest);
+
+// stopBtn.addEventListener('click', stopTest);
--- main.js
+++ main.js
@@ -1,47 +1,24 @@
-console.log('Test');
-
 const startBtn = document.querySelector('.start-btn');
 const stopBtn = document.querySelector('.stop-btn');
 const myCounter = document.querySelector('.my-counter');
-// const paragraphTimer = document.createElement('.paragraphTimer');
 const paragr = document.querySelector('.paragraphTimer');
+let counter = 11;
 
-// const div = document.querySelector('.my-counter')
 myCounter.appendChild(paragr);
 
-
-let counter = 11;
 function startTest() {
     updateCounter(counter--);
 }
-
 function updateCounter(count) {
+    setInterval(startTest, 1000);
     if (counter < 10){
         paragr.innerHTML = `25:0${counter}`;
-    } else {
-
-        paragr.innerHTML = `25:${counter}`;
     }
-    if (counter === 0) {
-
-        clearInterval();
+    if (counter <= 0) {
+        counter = 0;
+        paragr.innerHTML = `25:0${counter}`;
     }
 }
 
-function stopTest() {
-    counter = 0;
-    return counter;
-}
 
-// function stopApp() {
-//     if (stopBtn) {
-//         clearInterval(startTest);
-//     }
-// }
-
-//  setInterval(startApp, 1000);
- setInterval(startTest, 1000);
-// startBtn.addEventListener('click', startApp);
 startBtn.addEventListener('click', startTest);
-
-stopBtn.addEventListener('click', stopTest);
