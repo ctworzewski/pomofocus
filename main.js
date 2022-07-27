d61ba45a39d94f5f92f5487ba4eb376a9720a43d
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-07-27 19:23:58 +0200

    Correct repair v4

--- main.js
+++ main.js
@@ -13,20 +13,20 @@ function startTest() {
 function updateCounter(count) {
     if (counter >= 10){
         paragr.innerHTML = `25:${counter}`;
-    } else if(counter<10) {
+    } else if(counter<=10 && counter >= 0) {
 
         paragr.innerHTML = `25:0${counter}`;
         
     }
- if(counter === 0 && counter >= 0) {
-        counter = 0;
-        paragr.innerHTML = `25:0${counter}`;
-        console.log('test');
-    } else {
-        return;
-    }
+//  if(counter === 0) {
+//         counter = 0;
+//         paragr.innerHTML = `25:0${counter}`;
+//         console.log('test');
+//     } else {
+//         return;
+//     }
 }
 
 
-
+setInterval(startTest, 1000);
 startBtn.addEventListener('click', startTest);
