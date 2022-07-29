f428f91d7a5f988ab827705fcf9c01cf460ae4d8
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-07-29 07:13:35 +0200

    Add features  method padStart to my timer

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 const startBtn = document.querySelector(".start-btn");
- const stopBtn = document.querySelector('.stop-btn');
+const stopBtn = document.querySelector(".stop-btn");
 const myCounter = document.querySelector(".my-counter");
 const paragr = document.querySelector(".paragraphTimer");
 
 myCounter.appendChild(paragr);
 
-let counterSec = 5;
+let counterSec = 65;
 let intervalId;
 function startTest() {
-    stopTest();
-    counterSec = 5;
+  stopTest();
+  counterSec = 65;
   updateCounter();
 }
 function updateCounter() {
-    counterSec--;
-
-  paragr.innerHTML = `${Math.floor(counterSec / 60)}:${counterSec % 60}`;
-if (counterSec === 0) {
+  counterSec--;
+  let startMinute = `${Math.floor(counterSec / 60)}`;
+  let startSecunde = `${Math.floor(counterSec % 60)}`;
+//   let test11 = `${Math.floor(counterSec / 60)}:${counterSec % 60}`;
+  paragr.innerHTML = `${startMinute.padStart(2, '0')}:${startSecunde.padStart(2, '0')}`;
+  if (counterSec === 0) {
     return;
-}
+  }
   intervalId = setTimeout(updateCounter, 1000);
 }
 
 function stopTest() {
-    clearTimeout(intervalId);
+  clearTimeout(intervalId);
 }
 startBtn.addEventListener("click", startTest);
 stopBtn.addEventListener("click", stopTest);
-
