bcaee486101852cbeb1543b898bd8f143b579693
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2022-08-02 19:35:06 +0200

    Fix formatting

--- main.js
+++ main.js
@@ -15,17 +15,17 @@ let intervalIdshortBreakBtn;
 let lastActiveButton;
 function changeActiveColor(activeButton) {
   if (lastActiveButton !== undefined && lastActiveButton !== activeButton) {
-    lastActiveButton.classList.remove('active')
+    lastActiveButton.classList.remove("active");
   }
-  activeButton.classList.add('active');
+  activeButton.classList.add("active");
   lastActiveButton = activeButton;
 }
 changeActiveColor(pomodoroBtn);
 
 // pomodoroBtn.style.backgroundColor = "blue";
 function startTest() {
-    // document.body.classList.toggle('pomodoro-btn');
-console.log(pomodoroBtn);
+  // document.body.classList.toggle('pomodoro-btn');
+  console.log(pomodoroBtn);
   counterSec = 11;
   stopTest();
   updateCounter();
@@ -36,14 +36,16 @@ function startShortBreak() {
   counterSecBreak = 6;
   stopTest();
   updateCounterShortBreakBtn();
-
 }
 
 function updateCounterShortBreakBtn() {
   // counterSecBreak--;
   let startMinuteBreak = `${Math.floor(counterSecBreak / 60)}`;
   let startSecundeBreak = `${Math.floor(counterSecBreak % 60)}`;
-  paragr.innerHTML = `${startMinuteBreak.padStart(2, "0")}:${startSecundeBreak.padStart(2, "0")}`;
+  paragr.innerHTML = `${startMinuteBreak.padStart(
+    2,
+    "0"
+  )}:${startSecundeBreak.padStart(2, "0")}`;
 
   if (counterSecBreak === 0) {
     return;
@@ -56,15 +58,15 @@ function updateCounter() {
   counterSec--;
   let startMinute = `${Math.floor(counterSec / 60)}`;
   let startSecunde = `${Math.floor(counterSec % 60)}`;
-  paragr.innerHTML = `${startMinute.padStart(2, "0")}:${startSecunde.padStart(2, "0")}`;
+  paragr.innerHTML = `${startMinute.padStart(2, "0")}:${startSecunde.padStart(
+    2,
+    "0"
+  )}`;
   if (counterSec === 0) {
     return;
   }
   intervalId = setTimeout(updateCounter, 1000);
-
-
 }
-
 
 function stopTest() {
   clearTimeout(intervalId);
@@ -82,29 +84,29 @@ function shortBreakBtnTest() {
 // skorzystałem ze źródła https://techstacker.com/count-number-of-clicks-on-an-element-with-javascript/
 // funkcja ma sprawdzać ilość kliknięć
 let manyClick = 0;
-const clicks = document.querySelector('.clicks');
-clicks.addEventListener('click', function() {
+const clicks = document.querySelector(".clicks");
+clicks.addEventListener("click", function () {
   manyClick = manyClick + 1;
   console.log(manyClick);
-  
+
   // na if'a sam wpadłem
   if (manyClick === 2) {
     shortBreakBtnTest();
   }
 });
 
-  startBtn.addEventListener("click", startTest);
-  stopBtn.addEventListener("click", stopTest);
-  
-  shortBreakBtn.addEventListener("click", shortBreakBtnTest);
-  
-  // function playMusic() {
-  //   let audioTest = new Audio('./sources/music.mp3');
-  //   if (startBtn) {
-  //     audioTest.play();
-  
-  //   }
-  //   else if (stopBtn) {
-  //     audioTest.pause();
-  //   }
-  // }
+startBtn.addEventListener("click", startTest);
+stopBtn.addEventListener("click", stopTest);
+
+shortBreakBtn.addEventListener("click", shortBreakBtnTest);
+
+// function playMusic() {
+//   let audioTest = new Audio('./sources/music.mp3');
+//   if (startBtn) {
+//     audioTest.play();
+
+//   }
+//   else if (stopBtn) {
+//     audioTest.pause();
+//   }
+// }
