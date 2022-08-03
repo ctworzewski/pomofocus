273cf9920d6011a1e99af3fb5da6b2a64c02355c
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-08-03 23:14:42 +0200

    Create ESModules for changeColor correct

--- src/changeColor.js
+++ src/changeColor.js
@@ -1,9 +1,12 @@
 let lastActiveButton;
+{
+  let lastActiveButton = 'test F2';
+}
 // changeActiveColor(pomodoroBtn);
 export function changeActiveColor(activeButton) {
   if (lastActiveButton !== undefined && lastActiveButton !== activeButton) {
     lastActiveButton.classList.remove("active");
   }
-  activeButton.classList.add("active");
+  activeButton.classList.add("ac tive");
   lastActiveButton = activeButton;
 }
